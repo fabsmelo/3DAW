88cf251a2427492af2034652dbce940d98cfe1b7
fabsmelo <80009420+fabsmelo@users.noreply.github.com>
2021-04-13 13:06:38 -0300

    Add files via upload

--- home.php
+++ home.php
@@ -6,24 +6,24 @@
     <title>Calculadora</title>
   </head>
   <body>
-    <h1>Calculadora</h1>
+    <div div class="mx-auto" style="width: 600px;">
 
-    <div class="container-fluid">
+      <h1 style="margin-bottom: 50px;">Calculadora</h1>
 
-      <form class="form-row" action="" method="GET">
+      <form class="form-row" method="GET" style="margin-bottom: 85px;">
 
           <div class="form-group col-md-6">
             <label for="num1">Primeiro valor</label>
             <input type="text" class="form-control num1" id="num1" name="num1" placeholder="Insira um numero">
           </div>
 
-          <div class="form-group col-md-6">
+          <div class="form-group col-md-6" style="margin-bottom: 40px;">
             <label for="num2">Segundo valor</label>
             <input type="text" class="form-control num2" id="num2" name="num2" placeholder="Insira um numero">
           </div>
 
-          <div class="form-group col-md-4">
-            <label for="operacao">Qual operação deseja executar</label>
+          <div class="form-group col-md-12" style="margin-bottom: 30px;">
+            <label for="operacao">Selecione a operação que deseja executar</label>
             <select id="operacao" class="form-control" name="operacao">
               <option selected>Escolher...</option>
               <option value="soma">Soma</option>
@@ -38,42 +38,42 @@
           </div>
 
       </form>
+      <h4 style="margin: 0 auto; width: 50%;">
+      <?php
 
-    </div>
+          include "calculadora.php";
 
-    <?php
+          if (!empty($_GET["operacao"])) {
 
-        include "calculadora.php";
+            $num1 = $_GET['num1'];
+            $num2 = $_GET['num2'];
+            $op = $_GET['operacao'];
 
-        if (!empty($_GET["operacao"])) {
+            if($op == "soma"){
+              echo "O resultado da soma é: ".soma($num1, $num2);
 
-          $num1 = $_GET['num1'];
-          $num2 = $_GET['num2'];
-          $op = $_GET['operacao'];
+            } else if($op == "subtracao"){
+              echo "O resultado da subtração é: ".sub($num1, $num2);
 
-          if($op == "soma"){
-            echo "O resultado da soma é: ".soma($num1, $num2);
+            } else if($op == "multiplicacao"){
+              echo "O resultado da multiplicação é: ".mult($num1, $num2);
 
-          } else if($op == "subtracao"){
-            echo "O resultado da subtração é: ".sub($num1, $num2);
+            } else if($op == "divisao"){
+              if($num2 != 0){
+                echo "O resultado da divisão é: ".div($num1, $num2);
 
-          } else if($op == "multiplicacao"){
-            echo "O resultado da multiplicação é: ".mult($num1, $num2);
+              } else {
+                echo "Não é possível realizar divisão por 0";
 
-          } else if($op == "divisao"){
-            if($num2 != 0){
-              echo "O resultado da divisão é: ".div($num1, $num2);
-
-            } else {
-              echo "Não é possível realizar divisão por 0";
+              }
 
             }
 
           }
 
-        }
-
-    ?>
+      ?>
+    </h4>
+    </div>
 
   </body>
 </html>
